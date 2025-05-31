ebc641157e02891e69c118cc3fcb6600e37e7ceb
User Name <pauloredz88@gmail.com>
2025-05-30 22:09:48 -0300

    Create Quests.lua

---
+++ Utils/BloxFruits/GameModules/Quests.lua
@@ -0,0 +1,862 @@
+return {
+  ForgottenQuest = {
+    {
+      LevelReq = 1425,
+      Name = "Sea Soldier",
+      Task = {
+        ["Sea Soldier"] = 8
+      }
+    },
+    {
+      LevelReq = 1450,
+      Name = "Water Fighter",
+      Task = {
+        ["Water Fighter"] = 8
+      }
+    },
+    {
+      LevelReq = 1475,
+      Name = "Tide Keeper",
+      Task = {
+        ["Tide Keeper"] = 1
+      }
+    }
+  },
+  HauntedQuest2 = {
+    {
+      LevelReq = 2025,
+      Name = "Demonic Soul",
+      Task = {
+        ["Demonic Soul"] = 8
+      }
+    },
+    {
+      LevelReq = 2050,
+      Name = "Posessed Mummy",
+      Task = {
+        ["Posessed Mummy"] = 8
+      }
+    }
+  },
+  SkyQuest = {
+    {
+      LevelReq = 150,
+      Name = "Sky Bandit",
+      Task = {
+        ["Sky Bandit"] = 7
+      }
+    },
+    {
+      LevelReq = 175,
+      Name = "Dark Master",
+      Task = {
+        ["Dark Master"] = 8
+      }
+    }
+  },
+  ImpelQuest = {
+    {
+      LevelReq = 220,
+      Name = "Warden",
+      Task = {
+        Warden = 1
+      }
+    },
+    {
+      LevelReq = 230,
+      Name = "Chief Warden",
+      Task = {
+        ["Chief Warden"] = 1
+      }
+    },
+    {
+      LevelReq = 240,
+      Name = "Swan",
+      Task = {
+        Swan = 1
+      }
+    }
+  },
+  TikiQuest2 = {
+    {
+      LevelReq = 2500,
+      Name = "Sun-kissed Warrior",
+      Task = {
+        ["Sun-kissed Warrior"] = 8
+      }
+    },
+    {
+      LevelReq = 2525,
+      Name = "Isle Champion",
+      Task = {
+        ["Isle Champion"] = 8
+      }
+    }
+  },
+  Area1Quest = {
+    {
+      LevelReq = 700,
+      Name = "Raider",
+      Task = {
+        Raider = 8
+      }
+    },
+    {
+      LevelReq = 725,
+      Name = "Mercenary",
+      Task = {
+        Mercenary = 8
+      }
+    },
+    {
+      LevelReq = 750,
+      Name = "Diamond",
+      Task = {
+        Diamond = 1
+      }
+    }
+  },
+  DeepForestIsland3 = {
+    {
+      LevelReq = 1775,
+      Name = "Fishman Raider",
+      Task = {
+        ["Fishman Raider"] = 8
+      }
+    },
+    {
+      LevelReq = 1800,
+      Name = "Fishman Captain",
+      Task = {
+        ["Fishman Captain"] = 8
+      }
+    }
+  },
+  CandyQuest1 = {
+    {
+      LevelReq = 2400,
+      Name = "Candy Pirate",
+      Task = {
+        ["Candy Pirate"] = 8
+      }
+    },
+    {
+      LevelReq = 2425,
+      Name = "Snow Demon",
+      Task = {
+        ["Snow Demon"] = 8
+      }
+    }
+  },
+  FireSideQuest = {
+    {
+      LevelReq = 1175,
+      Name = "Magma Ninja",
+      Task = {
+        ["Magma Ninja"] = 8
+      }
+    },
+    {
+      LevelReq = 1200,
+      Name = "Lava Pirate",
+      Task = {
+        ["Lava Pirate"] = 8
+      }
+    }
+  },
+  JungleQuest = {
+    {
+      LevelReq = 10,
+      Name = "Monkeys",
+      Task = {
+        Monkey = 6
+      }
+    },
+    {
+      LevelReq = 15,
+      Name = "Gorillas",
+      Task = {
+        Gorilla = 8
+      }
+    },
+    {
+      LevelReq = 20,
+      Name = "Gorilla King",
+      Task = {
+        ["The Gorilla King"] = 1
+      }
+    }
+  },
+  FountainQuest = {
+    {
+      LevelReq = 625,
+      Name = "Galley Pirate",
+      Task = {
+        ["Galley Pirate"] = 8
+      }
+    },
+    {
+      LevelReq = 650,
+      Name = "Galley Captain",
+      Task = {
+        ["Galley Captain"] = 9
+      }
+    },
+    {
+      LevelReq = 675,
+      Name = "Cyborg",
+      Task = {
+        Cyborg = 1
+      }
+    }
+  },
+  DesertQuest = {
+    {
+      LevelReq = 60,
+      Name = "Desert Bandit",
+      Task = {
+        ["Desert Bandit"] = 8
+      }
+    },
+    {
+      LevelReq = 75,
+      Name = "Desert Officer",
+      Task = {
+        ["Desert Officer"] = 6
+      }
+    }
+  },
+  SnowQuest = {
+    {
+      LevelReq = 90,
+      Name = "Snow Bandit",
+      Task = {
+        ["Snow Bandit"] = 7
+      }
+    },
+    {
+      LevelReq = 100,
+      Name = "Snowman",
+      Task = {
+        Snowman = 8
+      }
+    },
+    {
+      LevelReq = 105,
+      Name = "Yeti",
+      Task = {
+        Yeti = 1
+      }
+    }
+  },
+  MarineTreeIsland = {
+    {
+      LevelReq = 1700,
+      Name = "Marine Commodore",
+      Task = {
+        ["Marine Commodore"] = 8
+      }
+    },
+    {
+      LevelReq = 1725,
+      Name = "Marine Rear Admiral",
+      Task = {
+        ["Marine Rear Admiral"] = 8
+      }
+    },
+    {
+      LevelReq = 1750,
+      Name = "Kilo Admiral",
+      Task = {
+        ["Kilo Admiral"] = 1
+      }
+    }
+  },
+  HauntedQuest1 = {
+    {
+      LevelReq = 1975,
+      Name = "Reborn Skeleton",
+      Task = {
+        ["Reborn Skeleton"] = 8
+      }
+    },
+    {
+      LevelReq = 2000,
+      Name = "Living Zombie",
+      Task = {
+        ["Living Zombie"] = 8
+      }
+    }
+  },
+  DeepForestIsland = {
+    {
+      LevelReq = 1825,
+      Name = "Forest Pirate",
+      Task = {
+        ["Forest Pirate"] = 8
+      }
+    },
+    {
+      LevelReq = 1850,
+      Name = "Mythological Pirate",
+      Task = {
+        ["Mythological Pirate"] = 8
+      }
+    },
+    {
+      LevelReq = 1875,
+      Name = "Captain Elephant",
+      Task = {
+        ["Captain Elephant"] = 1
+      }
+    }
+  },
+  BuggyQuest1 = {
+    {
+      LevelReq = 30,
+      Name = "Pirates",
+      Task = {
+        Pirate = 8
+      }
+    },
+    {
+      LevelReq = 40,
+      Name = "Brute",
+      Task = {
+        Brute = 8
+      }
+    },
+    {
+      LevelReq = 55,
+      Name = "Bobby",
+      Task = {
+        Bobby = 1
+      }
+    }
+  },
+  Area2Quest = {
+    {
+      LevelReq = 775,
+      Name = "Swan Pirate",
+      Task = {
+        ["Swan Pirate"] = 8
+      }
+    },
+    {
+      LevelReq = 800,
+      Name = "Factory Staff",
+      Task = {
+        ["Factory Staff"] = 8
+      }
+    },
+    {
+      LevelReq = 850,
+      Name = "Jeremy",
+      Task = {
+        Jeremy = 1
+      }
+    }
+  },
+  SkyExp1Quest = {
+    {
+      LevelReq = 450,
+      Name = "God's Guard",
+      Task = {
+        ["God's Guard"] = 7
+      }
+    },
+    {
+      LevelReq = 475,
+      Name = "Shanda",
+      Task = {
+        Shanda = 9
+      }
+    },
+    {
+      LevelReq = 500,
+      Name = "Wysper",
+      Task = {
+        Wysper = 1
+      }
+    }
+  },
+  ZombieQuest = {
+    {
+      LevelReq = 950,
+      Name = "Zombie",
+      Task = {
+        Zombie = 8
+      }
+    },
+    {
+      LevelReq = 975,
+      Name = "Vampire",
+      Task = {
+        Vampire = 8
+      }
+    }
+  },
+  CakeQuest2 = {
+    {
+      LevelReq = 2250,
+      Name = "Baking Staff",
+      Task = {
+        ["Baking Staff"] = 8
+      }
+    },
+    {
+      LevelReq = 2275,
+      Name = "Head Baker",
+      Task = {
+        ["Head Baker"] = 8
+      }
+    }
+  },
+  SnowMountainQuest = {
+    {
+      LevelReq = 1000,
+      Name = "Snow Trooper",
+      Task = {
+        ["Snow Trooper"] = 8
+      }
+    },
+    {
+      LevelReq = 1050,
+      Name = "Winter Warrior",
+      Task = {
+        ["Winter Warrior"] = 9
+      }
+    }
+  },
+  ShipQuest1 = {
+    {
+      LevelReq = 1250,
+      Name = "Ship Deckhand",
+      Task = {
+        ["Ship Deckhand"] = 8
+      }
+    },
+    {
+      LevelReq = 1275,
+      Name = "Ship Engineer",
+      Task = {
+        ["Ship Engineer"] = 8
+      }
+    }
+  },
+  DeepForestIsland2 = {
+    {
+      LevelReq = 1900,
+      Name = "Jungle Pirate",
+      Task = {
+        ["Jungle Pirate"] = 8
+      }
+    },
+    {
+      LevelReq = 1925,
+      Name = "Musketeer Pirate",
+      Task = {
+        ["Musketeer Pirate"] = 8
+      }
+    },
+    {
+      LevelReq = 1950,
+      Name = "Beautiful Pirate",
+      Task = {
+        ["Beautiful Pirate"] = 1
+      }
+    }
+  },
+  FrostQuest = {
+    {
+      LevelReq = 1350,
+      Name = "Arctic Warrior",
+      Task = {
+        ["Arctic Warrior"] = 8
+      }
+    },
+    {
+      LevelReq = 1375,
+      Name = "Snow Lurker",
+      Task = {
+        ["Snow Lurker"] = 8
+      }
+    },
+    {
+      LevelReq = 1400,
+      Name = "Ice Admiral",
+      Task = {
+        ["Awakened Ice Admiral"] = 1
+      }
+    }
+  },
+  PiratePortQuest = {
+    {
+      LevelReq = 1500,
+      Name = "Pirate Millionaire",
+      Task = {
+        ["Pirate Millionaire"] = 8
+      }
+    },
+    {
+      LevelReq = 1525,
+      Name = "Pistol Billionaire",
+      Task = {
+        ["Pistol Billionaire"] = 8
+      }
+    },
+    {
+      LevelReq = 1550,
+      Name = "Stone",
+      Task = {
+        Stone = 1
+      }
+    }
+  },
+  PrisonerQuest = {
+    {
+      LevelReq = 190,
+      Name = "Prisoner",
+      Task = {
+        Prisoner = 8
+      }
+    },
+    {
+      LevelReq = 210,
+      Name = "Dangerous Prisoner",
+      Task = {
+        ["Dangerous Prisoner"] = 8
+      }
+    }
+  },
+  MarineQuest3 = {
+    {
+      LevelReq = 875,
+      Name = "Marine Lieutenant",
+      Task = {
+        ["Marine Lieutenant"] = 8
+      }
+    },
+    {
+      LevelReq = 900,
+      Name = "Marine Captain",
+      Task = {
+        ["Marine Captain"] = 9
+      }
+    },
+    {
+      LevelReq = 925,
+      Name = "Fajita",
+      Task = {
+        Fajita = 1
+      }
+    }
+  },
+  BanditQuest1 = {
+    {
+      LevelReq = 0,
+      Name = "Bandits",
+      Task = {
+        Bandit = 5
+      }
+    }
+  },
+  DragonCrewQuest = {
+    {
+      LevelReq = 1575,
+      Name = "Dragon Crew Warrior",
+      Task = {
+        ["Dragon Crew Warrior"] = 8
+      }
+    },
+    {
+      LevelReq = 1600,
+      Name = "Dragon Crew Archer",
+      Task = {
+        ["Dragon Crew Archer"] = 8
+      }
+    }
+  },
+  MagmaQuest = {
+    {
+      LevelReq = 300,
+      Name = "Mil. Soldier",
+      Task = {
+        ["Military Soldier"] = 7
+      }
+    },
+    {
+      LevelReq = 325,
+      Name = "Mil. Spy",
+      Task = {
+        ["Military Spy"] = 8
+      }
+    },
+    {
+      LevelReq = 350,
+      Name = "Magma Admiral",
+      Task = {
+        ["Magma Admiral"] = 1
+      }
+    }
+  },
+  VenomCrewQuest = {
+    {
+      LevelReq = 1625,
+      Name = "Hydra Enforcer",
+      Task = {
+        ["Hydra Enforcer"] = 8
+      }
+    },
+    {
+      LevelReq = 1650,
+      Name = "Venomous Assailant",
+      Task = {
+        ["Venomous Assailant"] = 8
+      }
+    },
+    {
+      LevelReq = 1675,
+      Name = "Hydra Leader",
+      Task = {
+        ["Hydra Leader"] = 1
+      }
+    }
+  },
+  CakeQuest1 = {
+    {
+      LevelReq = 2200,
+      Name = "Cookie Crafter",
+      Task = {
+        ["Cookie Crafter"] = 8
+      }
+    },
+    {
+      LevelReq = 2225,
+      Name = "Cake Guard",
+      Task = {
+        ["Cake Guard"] = 8
+      }
+    }
+  },
+  TikiQuest3 = {
+    {
+      LevelReq = 2550,
+      Name = "Serpent Hunter",
+      Task = {
+        ["Serpent Hunter"] = 8
+      }
+    },
+    {
+      LevelReq = 2575,
+      Name = "Skull Slayer",
+      Task = {
+        ["Skull Slayer"] = 8
+      }
+    }
+  },
+  TikiQuest1 = {
+    {
+      LevelReq = 2450,
+      Name = "Isle Outlaw",
+      Task = {
+        ["Isle Outlaw"] = 8
+      }
+    },
+    {
+      LevelReq = 2475,
+      Name = "Island Boy",
+      Task = {
+        ["Island Boy"] = 8
+      }
+    }
+  },
+  SkyExp2Quest = {
+    {
+      LevelReq = 525,
+      Name = "Royal Squad",
+      Task = {
+        ["Royal Squad"] = 8
+      }
+    },
+    {
+      LevelReq = 550,
+      Name = "Royal Soldier",
+      Task = {
+        ["Royal Soldier"] = 8
+      }
+    },
+    {
+      LevelReq = 575,
+      Name = "Thunder God",
+      Task = {
+        ["Thunder God"] = 1
+      }
+    }
+  },
+  MarineQuest2 = {
+    {
+      LevelReq = 120,
+      Name = "Chief Petty Officer",
+      Task = {
+        ["Chief Petty Officer"] = 8
+      }
+    },
+    {
+      LevelReq = 130,
+      Name = "Vice Admiral",
+      Task = {
+        ["Vice Admiral"] = 1
+      }
+    }
+  },
+  IceSideQuest = {
+    {
+      LevelReq = 1100,
+      Name = "Lab Subordinate",
+      Task = {
+        ["Lab Subordinate"] = 8
+      }
+    },
+    {
+      LevelReq = 1125,
+      Name = "Horned Warrior",
+      Task = {
+        ["Horned Warrior"] = 9
+      }
+    },
+    {
+      LevelReq = 1150,
+      Name = "Smoke Admiral",
+      Task = {
+        ["Smoke Admiral"] = 1
+      }
+    }
+  },
+  ColosseumQuest = {
+    {
+      LevelReq = 250,
+      Name = "Toga Warrior",
+      Task = {
+        ["Toga Warrior"] = 7
+      }
+    },
+    {
+      LevelReq = 275,
+      Name = "Gladiator",
+      Task = {
+        Gladiator = 8
+      }
+    }
+  },
+  ShipQuest2 = {
+    {
+      LevelReq = 1300,
+      Name = "Ship Steward",
+      Task = {
+        ["Ship Steward"] = 8
+      }
+    },
+    {
+      LevelReq = 1325,
+      Name = "Ship Officer",
+      Task = {
+        ["Ship Officer"] = 8
+      }
+    }
+  },
+  NutsIslandQuest = {
+    {
+      LevelReq = 2075,
+      Name = "Peanut Scout",
+      Task = {
+        ["Peanut Scout"] = 8
+      }
+    },
+    {
+      LevelReq = 2100,
+      Name = "Peanut President",
+      Task = {
+        ["Peanut President"] = 8
+      }
+    }
+  },
+  IceCreamIslandQuest = {
+    {
+      LevelReq = 2125,
+      Name = "Ice Cream Chef",
+      Task = {
+        ["Ice Cream Chef"] = 8
+      }
+    },
+    {
+      LevelReq = 2150,
+      Name = "Ice Cream Commander",
+      Task = {
+        ["Ice Cream Commander"] = 8
+      }
+    },
+    {
+      LevelReq = 2175,
+      Name = "Cake Queen",
+      Task = {
+        ["Cake Queen"] = 1
+      }
+    }
+  },
+  FishmanQuest = {
+    {
+      LevelReq = 375,
+      Name = "Fishman Warrior",
+      Task = {
+        ["Fishman Warrior"] = 8
+      }
+    },
+    {
+      LevelReq = 400,
+      Name = "Fishman Commando",
+      Task = {
+        ["Fishman Commando"] = 7
+      }
+    },
+    {
+      LevelReq = 425,
+      Name = "Fishman Lord",
+      Task = {
+        ["Fishman Lord"] = 1
+      }
+    }
+  },
+  ChocQuest1 = {
+    {
+      LevelReq = 2300,
+      Name = "Cocoa Warrior",
+      Task = {
+        ["Cocoa Warrior"] = 8
+      }
+    },
+    {
+      LevelReq = 2325,
+      Name = "Chocolate Bar Battler",
+      Task = {
+        ["Chocolate Bar Battler"] = 8
+      }
+    }
+  },
+  ChocQuest2 = {
+    {
+      LevelReq = 2350,
+      Name = "Sweet Thief",
+      Task = {
+        ["Sweet Thief"] = 8
+      }
+    },
+    {
+      LevelReq = 2375,
+      Name = "Candy Rebel",
+      Task = {
+        ["Candy Rebel"] = 8
+      }
+    }
+  }
+}
